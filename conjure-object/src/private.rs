cff29ce8e3365f242882fc76977977a7740d9795
Steven Fackler <sfackler@palantir.com>
2024-02-15 09:57:20 -0500

    fix build

--- conjure-object/src/private.rs
+++ conjure-object/src/private.rs
@@ -173,6 +173,11 @@ where
     V: DoubleOps,
 {
     #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+
+    #[inline]
     fn cmp(&self, other: &Self) -> Ordering {
         self.iter()
             .map(|(k, v)| (k, DoubleOpsWrapper(v)))
