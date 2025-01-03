14668e5319f6887524adadd8575b24c2cab71cd9
Steven Fackler <sfackler@palantir.com>
2025-01-02 20:20:21 -0500

    Derive Serialize and Deserialize for enums

--- conjure-codegen/src/types/http_method.rs
+++ conjure-codegen/src/types/http_method.rs
@@ -1,11 +1,26 @@
-use conjure_object::serde::{ser, de};
+#![allow(deprecated)]
 use std::fmt;
 use std::str;
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    conjure_object::serde::Deserialize,
+    conjure_object::serde::Serialize,
+)]
+#[serde(crate = "conjure_object::serde")]
 pub enum HttpMethod {
+    #[serde(rename = "GET")]
     Get,
+    #[serde(rename = "POST")]
     Post,
+    #[serde(rename = "PUT")]
     Put,
+    #[serde(rename = "DELETE")]
     Delete,
 }
 impl HttpMethod {
@@ -48,39 +63,5 @@ impl conjure_object::FromPlain for HttpMethod {
     #[inline]
     fn from_plain(v: &str) -> Result<HttpMethod, conjure_object::plain::ParseEnumError> {
         v.parse()
-    }
-}
-impl ser::Serialize for HttpMethod {
-    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
-    where
-        S: ser::Serializer,
-    {
-        s.serialize_str(self.as_str())
-    }
-}
-impl<'de> de::Deserialize<'de> for HttpMethod {
-    fn deserialize<D>(d: D) -> Result<HttpMethod, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        d.deserialize_str(Visitor_)
-    }
-}
-struct Visitor_;
-impl<'de> de::Visitor<'de> for Visitor_ {
-    type Value = HttpMethod;
-    fn expecting(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.write_str("a string")
-    }
-    fn visit_str<E>(self, v: &str) -> Result<HttpMethod, E>
-    where
-        E: de::Error,
-    {
-        match v.parse() {
-            Ok(e) => Ok(e),
-            Err(_) => {
-                Err(de::Error::unknown_variant(v, &["GET", "POST", "PUT", "DELETE"]))
-            }
-        }
     }
 }
