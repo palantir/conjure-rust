bec0e5e5d49107004383a29fae160df36b78267b
Steven Fackler <sfackler@palantir.com>
2025-03-14 13:09:12 -0400

    Fix dependency cycle

--- conjure-macros/src/lib.rs
+++ conjure-macros/src/lib.rs
@@ -14,22 +14,6 @@
 //! Macros exposed by conjure-http.
 //!
 //! Do not consume directly.
-// Copyright 2022 Palantir Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//! Macros exposed by conjure-http.
-//!
-//! Do not consume directly.
 #![warn(missing_docs)]
 
 use proc_macro::TokenStream;
@@ -109,7 +93,7 @@ mod path;
 ///
 /// # Examples
 ///
-/// ```rust
+/// ```rust,ignore
 /// use conjure_error::Error;
 /// use conjure_http::{conjure_client, endpoint};
 /// use conjure_http::client::{
@@ -313,7 +297,7 @@ pub fn conjure_client(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///
 /// # Examples
 ///
-/// ```rust
+/// ```rust,ignore
 /// use conjure_error::Error;
 /// use conjure_http::{conjure_endpoints, endpoint};
 /// use conjure_http::server::{
